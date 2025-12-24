a9c0398faa035d338147f701b20eab92b29f1561
abraham <abrahamkifle1212@gmail.com>
2025-12-24 14:19:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {FiSettings } from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import './App.css'
+
+function App() {
+
+  return (
+    <div></div>
+  )
+}
+
+export default App
