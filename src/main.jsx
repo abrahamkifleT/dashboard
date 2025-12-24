1a2761b8c8d1a8aa6c164e986f461158a565aabc
abraham <abrahamkifle1212@gmail.com>
2025-12-24 16:07:56 +0300

    setup app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerLicense } from '@syncfusion/ej2-base'
 import './index.css'
 import App from './App.jsx'
+
+// Syncfusion CSS Imports
+import '@syncfusion/ej2-base/styles/material.css';
+import '@syncfusion/ej2-buttons/styles/material.css';
+import '@syncfusion/ej2-popups/styles/material.css';
+import '@syncfusion/ej2-react-popups/styles/material.css';
+
+// Registering Syncfusion license key
+registerLicense("Ngo9BigBOggjHTQxAR8/V1JGaF5cX2NCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWH1cdHRTRWRYWUFxVkVWYEs=");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
