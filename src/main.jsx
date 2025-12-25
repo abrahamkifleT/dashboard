7f90e92b1bb7e8e590779c25b077803b710f9b10
abraham <abrahamkifle1212@gmail.com>
2025-12-25 22:35:39 +0300

    feat: add Kanban board component and integrate Syncfusion license and styles.

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,9 @@ import '@syncfusion/ej2-base/styles/material.css';
 import '@syncfusion/ej2-buttons/styles/material.css';
 import '@syncfusion/ej2-popups/styles/material.css';
 import '@syncfusion/ej2-react-popups/styles/material.css';
+import '@syncfusion/ej2-react-kanban/styles/material.css';
+import '@syncfusion/ej2-react-inputs/styles/material.css';
+import '@syncfusion/ej2-react-dropdowns/styles/material.css';
 
 // Registering Syncfusion license key
 registerLicense("Ngo9BigBOggjHTQxAR8/V1JGaF5cX2NCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWH1cdHRTRWRYWUFxVkVWYEs=");
